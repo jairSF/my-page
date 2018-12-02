ba0240cff2495e3e9344eb97aa1bb41f74c767f6
jair silva <jairsilva@MacBook-Pro-de-jair.local>
2018-12-02 00:54:32 -0500

    formateando css

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,18 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
+
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hello I'm Jair Silva a Product Designer</h1>
-    <p>I have always had a fascination with the convergence of art, design, music, technology and life. I am currently looking for a job that will enable me to achieve my goals.
-      I graduated from the Higher Institute of Communication and Design Toulouse Lautrec in the career of Web Design and Multimedia. I am passionate about branding, development from web design to programming, UX / UI and interactive development.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+    <div className="Hero">
+       <div className="heroGroup">
+          <h1>Hello I'm Jair Silva a Product Designer</h1>
+          <p>I have always had a fascination with the convergence of art, design, music, technology and life. I am currently looking for a job that will enable me to achieve my goals.
+          I graduated from the Higher Institute of Communication and Design Toulouse Lautrec in the career of Web Design and Multimedia. I am passionate about branding, development from web design to programming, UX / UI and interactive development.</p>
+          <Link to="/page-2/">About</Link>
+       </div>
     </div>
-    <Link to="/page-2/">About us</Link>
   </Layout>
 )
 
